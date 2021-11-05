6485904cb9d2ccd42e433d7419336f9fefd34dd2
Annie Cherkaev <annie.cherk@gmail.com>
2021-11-04 18:33:47 -0600

    bugfix: moved WHICHSTAR constant below the function it needs to reference

--- stars.py
+++ stars.py
@@ -3,10 +3,6 @@ from pathlib import Path
 
 from utils import *
 
-# toggle to switch which function is run
-WHICHSTAR = star1
-INPUT_FILE = str(Path.cwd()) + "/input.txt"
-
 # sample input from the problem
 SAMPLE = r"""
 
@@ -27,10 +23,17 @@ def star2(puzzle: str) -> str:
 # print(star2(SAMPLE))
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# toggle to switch which function is run
+WHICHSTAR = star1
+INPUT_FILE = str(Path.cwd()) + "/input.txt"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:  # we're running on the example input
         print(f"\n\n {WHICHSTAR(SAMPLE)} \n\n")
     else:  # otherwise, get the input
         with open(INPUT_FILE) as puzzle_input:
-            print(f"\n\n {WHICHSTAR(puzzle_input.read())} \n\n")
+            print(f"\n\n {WHICHSTAR(puzzle_input.read().strip())} \n\n")
     print("day TODO, done")
